1e6b351c2d7e21687d4b6dd0b7aec73ed5d477b5
VadimBaby <vadimdim22@gmail.com>
2023-11-01 20:54:57 +0700

    Created project

---
+++ TraceNBA/TraceNBAApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceNBAApp.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 01.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TraceNBAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
