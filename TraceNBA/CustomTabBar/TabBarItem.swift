3e0894fb813a13fc4b9574440c87b40850971703
VadimBaby <vadimdim22@gmail.com>
2023-11-01 22:15:32 +0700

    Created TabItem

---
+++ TraceNBA/CustomTabBar/TabBarItem.swift
@@ -0,0 +1,13 @@
+//
+//  TabBarItem.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 01.11.2023.
+//
+
+import Foundation
+
+struct TabItem: Hashable {
+    let title: String
+    let iconName: String
+}
