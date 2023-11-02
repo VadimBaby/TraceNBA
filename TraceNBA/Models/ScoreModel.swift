d76fd319eac7e6208f230e9a1b4915f77c4348dc
VadimBaby <vadimdim22@gmail.com>
2023-11-02 14:19:00 +0700

    Added inits in ScoreModel

--- TraceNBA/Models/ScoreModel.swift
+++ TraceNBA/Models/ScoreModel.swift
@@ -8,10 +8,46 @@
 import Foundation
 
 struct ScoreModel: Codable {
-    let current: Int
-    let display: Int
-    let period1: Int
-    let period2: Int
-    let period3: Int
-    let period4: Int
+    let current: Int?
+    let display: Int?
+    let period1: Int?
+    let period2: Int?
+    let period3: Int?
+    let period4: Int?
+    
+    init(current: Int, display: Int, period1: Int) {
+        self.current = current
+        self.display = display
+        self.period1 = period1
+        self.period2 = nil
+        self.period3 = nil
+        self.period4 = nil
+    }
+    
+    init(current: Int, display: Int, period1: Int, period2: Int?) {
+        self.current = current
+        self.display = display
+        self.period1 = period1
+        self.period2 = period2
+        self.period3 = nil
+        self.period4 = nil
+    }
+    
+    init(current: Int, display: Int, period1: Int, period2: Int?, period3: Int?) {
+        self.current = current
+        self.display = display
+        self.period1 = period1
+        self.period2 = period2
+        self.period3 = period3
+        self.period4 = nil
+    }
+    
+    init(current: Int, display: Int, period1: Int, period2: Int?, period3: Int?, period4: Int?) {
+        self.current = current
+        self.display = display
+        self.period1 = period1
+        self.period2 = period2
+        self.period3 = period3
+        self.period4 = period4
+    }
 }
