3804e1b5681fe6e077fba4664bc14306f3275055
VadimBaby <vadimdim22@gmail.com>
2023-11-02 13:47:02 +0700

    Created StreamMatchesViewModel

--- TraceNBA/ViewModels/StreamMatchesViewModel.swift
+++ TraceNBA/ViewModels/StreamMatchesViewModel.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+class StreamMatchesViewModel: ObservableObject {
+    @Published public var listLiveMatches: [MatchModel] = []
+    
+    private let manager: DataServiceProtocol
+    
+    private var tasks: [Task<Void, Never>] = []
+    
+    init(manager: DataServiceProtocol) {
+        self.manager = manager
+    }
+    
+    func getListLiveMatches() {
+        let task1 = Task {
+            do {
+                let events = try await getLiveMatches()
+                
+                await MainActor.run {
+                    listLiveMatches = events
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        tasks.append(task1)
+    }
+    
+    private func getLiveMatches() async throws -> [MatchModel] {
+        let data = try await manager.getLiveMatchesData()
+        
+        let decodeData = try JSONDecoder().decode(DataModel.self, from: data)
+        
+        guard let events = decodeData.events else { throw URLError(.badServerResponse) }
+        
+        return events
+    }
+    
+    func cancelAllTasks() {
+        tasks.forEach{ $0.cancel() }
+        tasks = []
+    }
+}
