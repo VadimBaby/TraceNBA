616b610d7130f29ab29482fa7ad2fbec8e80eb43
VadimBaby <vadimdim22@gmail.com>
2023-11-02 22:57:12 +0700

    Created tests for MockDataService in MockDataService_Tests

--- TraceNBA_Tests/MockDataService_Tests.swift
+++ TraceNBA_Tests/MockDataService_Tests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+@testable import TraceNBA
 
 final class MockDataService_Tests: XCTestCase {
 
@@ -17,19 +18,39 @@ final class MockDataService_Tests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_MockDataService_getLiveMatchesData_shouldReturnDataThatPassedIn() {
+        let data: [Int] = [1, 2 ,3]
+        
+        let manager = MockDataService<[Int]>(data: data)
+        
+        do {
+            let encodeData = try JSONEncoder().encode(data)
+            
+            Task {
+                let dataFromManager = try await manager.getLiveMatchesData()
+                
+                XCTAssertEqual(dataFromManager, encodeData)
+            }
+        } catch {
+            XCTFail()
         }
     }
 
+    func test_MockDataService_getLiveMatchesData_shouldReturnAnotherData() {
+        let manager = MockDataService<[Int]>(data: [1, 2 ,3])
+        
+        let data: [Int] = [1, 2 ,3, 4]
+        
+        do {
+            let encodeData = try JSONEncoder().encode(data)
+            
+            Task {
+                let dataFromManager = try await manager.getLiveMatchesData()
+                
+                XCTAssertNotEqual(dataFromManager, encodeData)
+            }
+        } catch {
+            XCTFail()
+        }
+    }
 }
