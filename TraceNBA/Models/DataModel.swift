4af1e79ed08c2cb5b217afa8fd02dc9a993140ad
VadimBaby <vadimdim22@gmail.com>
2023-11-02 15:21:41 +0700

    Added text parameter

--- TraceNBA/Models/DataModel.swift
+++ TraceNBA/Models/DataModel.swift
@@ -10,14 +10,24 @@ import Foundation
 struct DataModel: Codable {
     let events: [MatchModel]?
     let players: [PlayerDataModel]?
+    let text: String?
     
     init(events: [MatchModel]) {
         self.events = events
         self.players = nil
+        self.text = nil
     }
     
     init(players: [PlayerDataModel]) {
         self.players = players
+        self.events = nil
+        self.text = nil
+    }
+    
+    
+    init(text: String) {
+        self.text = text
+        self.players = nil
         self.events = nil
     }
 }
