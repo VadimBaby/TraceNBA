98be249a08829b2e0463df5d7137eca2d4027d8c
VadimBaby <vadimdim22@gmail.com>
2023-11-02 23:53:55 +0700

    Created LocalFileManager

---
+++ TraceNBA/Services/LocalFileManager.swift
@@ -0,0 +1,127 @@
+//
+//  LocalFileManager.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 02.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+enum TypeFolder: String {
+    case teams
+    case players
+    case managers
+}
+
+class LocalFileManager {
+    static let instance = LocalFileManager()
+    
+    let folderTeams = "teams"
+    let folderPlayers = "players"
+    let folderManagers = "managers"
+    
+    func saveImage(image: UIImage, name: String, typeFolder: TypeFolder) -> String {
+        createFolderIfNeeded(typeFolder: typeFolder)
+        
+        guard
+            let data = image.jpegData(compressionQuality: 1.0),
+            let path = getPathForImage(name: name, typeFolder: typeFolder) else {
+            return "Error getting data"
+        }
+        
+        do {
+            try data.write(to: path)
+            return "Success saving"
+        } catch let error {
+            return "error: \(error)"
+        }
+    }
+    
+    func getImage(name: String, typeFolder: TypeFolder) -> UIImage? {
+        
+        guard
+            let path = getPathForImage(name: name, typeFolder: typeFolder)?.path(),
+            FileManager.default.fileExists(atPath: path) else {
+            print("error getting path")
+            return nil
+        }
+        
+        return UIImage(contentsOfFile: path)
+    }
+    
+    private func createFolderIfNeeded(typeFolder: TypeFolder) {
+        
+        let folderName = typeFolder.rawValue
+        
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName, conformingTo: .folder)
+                .path() else { return }
+        
+        if !FileManager.default.fileExists(atPath: path) {
+            do {
+                try FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: true)
+                print("success creating folder")
+            } catch let error {
+                print("error creating folder: \(error)")
+            }
+        }
+    }
+    
+    private func deleteFolder(typeFolder: TypeFolder) {
+        
+        let folderName = typeFolder.rawValue
+        
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName, conformingTo: .folder)
+                .path() else { return }
+        
+        do {
+            try FileManager.default.removeItem(atPath: path)
+            print("Success deleting folder")
+        } catch let error {
+            print("Error deleting folder: \(error)")
+        }
+    }
+    
+    func deleteImage(name: String, typeFolder: TypeFolder) -> String {
+        guard
+            let path = getPathForImage(name: name, typeFolder: typeFolder)?.path(),
+            FileManager.default.fileExists(atPath: path) else {
+            return "error getting path"
+        }
+         
+        do{
+            try FileManager.default.removeItem(atPath: path)
+            return "successfully deleted"
+        } catch let error {
+            return "Error deleting images: \(error)"
+        }
+    }
+    
+    private func getPathForImage(name: String, typeFolder: TypeFolder) -> URL? {
+        
+        let folderName = typeFolder.rawValue
+        
+        guard
+            let path = FileManager
+                .default
+                .urls(for: .cachesDirectory, in: .userDomainMask)
+                .first?
+                .appendingPathComponent(folderName, conformingTo: .folder)
+                .appendingPathComponent("\(name).jpg", conformingTo: .image) else {
+            print("Error getting path")
+            return nil
+        }
+        
+        return path
+    }
+}
