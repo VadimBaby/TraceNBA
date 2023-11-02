f11f81032950142997f0593599f277bb4d7a41bd
VadimBaby <vadimdim22@gmail.com>
2023-11-02 13:18:46 +0700

    Removed .frame from LiveComponent

--- TraceNBA/Components/LiveComponent.swift
+++ TraceNBA/Components/LiveComponent.swift
@@ -11,18 +11,18 @@ struct LiveComponent: View {
     var body: some View {
         RoundedRectangle(cornerRadius: 15)
             .fill(Color.red)
-            .frame(width: 250, height: 65)
             .overlay {
                 HStack {
                     Text("LIVE")
                     Image(systemName: "play.circle.fill")
                 }
                 .foregroundStyle(Color.white)
-                .font(.system(.largeTitle, design: .rounded, weight: .bold))
+                .font(.system(.title, design: .rounded, weight: .bold))
             }
     }
 }
 
 #Preview {
     LiveComponent()
+        .frame(width: 250, height: 50)
 }
