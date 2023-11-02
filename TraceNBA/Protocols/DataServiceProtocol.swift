d76a19ebcb7f8b6929f47fd223ad31acccdfcbf0
VadimBaby <vadimdim22@gmail.com>
2023-11-02 12:22:00 +0700

    Created DataServiceProtocol

---
+++ TraceNBA/Protocols/DataServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  DataServiceProtocol.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 02.11.2023.
+//
+
+import Foundation
+
+protocol DataServiceProtocol {
+    func getLiveMatchesData(urlString: String) async throws -> Data
+}
