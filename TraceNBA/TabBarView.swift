7675c66045d508877c2aa727745607f5c342a4de
VadimBaby <vadimdim22@gmail.com>
2023-11-02 12:54:55 +0700

    Created LinearGradient as background of app, also created FirstGradientColor and SecondGradientColor in Assets

--- TraceNBA/TabBarView.swift
+++ TraceNBA/TabBarView.swift
@@ -11,40 +11,48 @@ struct TabBarView: View {
     
     @State private var selection: TabItem = TabItem(title: "Home", iconName: "house")
     
+    let firstGradientColor: Color = Color("FirstGradientColor")
+    let secondGradientColor: Color = Color("SecondGradientColor")
+    
     var body: some View {
-        CustomTabBarContainerView(
-            selection: $selection,
-            iconColor: Color.green
-        ) {
-            Text("Home")
-                .tabBarItem (
-                    tab: TabItem(title: "Home", iconName: "house"),
-                    selection: $selection
-                )
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [firstGradientColor, secondGradientColor]), startPoint: .leading, endPoint: .trailing)
+                .ignoresSafeArea()
             
-            Text("Search")
-                .tabBarItem (
-                    tab: TabItem(title: "Search", iconName: "magnifyingglass"),
-                    selection: $selection
-                )
-            
-            StreamMatchesView()
-                .tabBarItem (
-                    tab: TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"),
-                    selection: $selection
-                )
-            
-            Text("Events")
-                .tabBarItem (
-                    tab: TabItem(title: "Events", iconName: "calendar"),
-                    selection: $selection
-                )
-            
-            Text("Settings")
-                .tabBarItem (
-                    tab: TabItem(title: "Settings", iconName: "gear"),
-                    selection: $selection
-                )
+            CustomTabBarContainerView(
+                selection: $selection,
+                iconColor: Color.green
+            ) {
+                Text("Home")
+                    .tabBarItem (
+                        tab: TabItem(title: "Home", iconName: "house"),
+                        selection: $selection
+                    )
+                
+                Text("Search")
+                    .tabBarItem (
+                        tab: TabItem(title: "Search", iconName: "magnifyingglass"),
+                        selection: $selection
+                    )
+                
+                StreamMatchesView()
+                    .tabBarItem (
+                        tab: TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"),
+                        selection: $selection
+                    )
+                
+                Text("Events")
+                    .tabBarItem (
+                        tab: TabItem(title: "Events", iconName: "calendar"),
+                        selection: $selection
+                    )
+                
+                Text("Settings")
+                    .tabBarItem (
+                        tab: TabItem(title: "Settings", iconName: "gear"),
+                        selection: $selection
+                    )
+            }
         }
     }
 }
