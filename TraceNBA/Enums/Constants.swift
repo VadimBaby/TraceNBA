edb66de9ff0801612f6edb281af2ac13144797fe
VadimBaby <vadimdim22@gmail.com>
2023-11-03 23:33:39 +0700

    Added 2 apiKey in constant rapidKeys

--- TraceNBA/Enums/Constants.swift
+++ TraceNBA/Enums/Constants.swift
@@ -10,6 +10,8 @@ import Foundation
 enum Constants {
     static let tournament: Int = 132
     static let rapidKeys: Set<String> = [
+        "f2996c39d6mshd0038ef0b5c5bfcp11fafdjsn1ce8657d5ec9",
+        "5b561445e9msh71e928590d6385fp161de1jsnd24aefb07b88",
         "eb8c8db184msh14e0af8492403fdp13d428jsnffa628acac2a",
         "0c86c0d6bfmsh04188a64979797ep1a6e69jsna9d9630decfd",
         "6e1559cd5cmshe03d10503cdd912p1a37bbjsn4f0eda215448",
