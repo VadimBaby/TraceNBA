9103d31d0f969b2c5e8d125fe3075c338159c8a0
VadimBaby <vadimdim22@gmail.com>
2023-11-03 18:21:09 +0700

    Added several rapid keys in constant rapidKeys in Constants

--- TraceNBA/Enums/Constants.swift
+++ TraceNBA/Enums/Constants.swift
@@ -9,5 +9,11 @@ import Foundation
 
 enum Constants {
     static let tournament: Int = 132
-    static let rapidKeys: Set<String> = ["0ee6652a40mshb2a06f6f8ed8e7fp129ce4jsnb16762573247"]
+    static let rapidKeys: Set<String> = [
+        "eb8c8db184msh14e0af8492403fdp13d428jsnffa628acac2a",
+        "0c86c0d6bfmsh04188a64979797ep1a6e69jsna9d9630decfd",
+        "6e1559cd5cmshe03d10503cdd912p1a37bbjsn4f0eda215448",
+        "988cae6b05mshdf3c117b5f5b58ap1d3ebfjsn290a68bd6831",
+        "0ee6652a40mshb2a06f6f8ed8e7fp129ce4jsnb16762573247"
+    ]
 }
