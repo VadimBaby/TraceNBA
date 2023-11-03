c7228c2045fec4bb1d303f832fa0f3efe9adfc5f
VadimBaby <vadimdim22@gmail.com>
2023-11-03 12:01:15 +0700

    Created TypeEntity and created getPhotoEntity func in DataService

--- TraceNBA/Services/DataService.swift
+++ TraceNBA/Services/DataService.swift
@@ -8,6 +8,23 @@
 import Foundation
 import Combine
 
+enum TypeEntity {
+    case team
+    case player
+    case manager
+    
+    func getUrlString(id: Int) -> String {
+        switch self {
+        case .team:
+            return "https://basketapi1.p.rapidapi.com/api/basketball/team/\(id)/image"
+        case .player:
+            return "https://basketapi1.p.rapidapi.com/api/basketball/player/\(id)/image"
+        case .manager:
+            return "https://basketapi1.p.rapidapi.com/api/basketball/manager/\(id)/image"
+        }
+    }
+}
+
 actor DataService: DataServiceProtocol {
     func getLiveMatchesData() async throws -> Data {
         
@@ -31,5 +48,26 @@ actor DataService: DataServiceProtocol {
         } catch {
             throw error
         }
+    }
+    
+    func getPhotoEntity(entity: TypeEntity, id: Int) async throws -> Data {
+        let urlString = entity.getUrlString(id: id)
+        
+        guard let url = URL(string: urlString) else { throw URLError(.badURL) }
+        
+        guard let apiKey = Constants.rapidKeys.randomElement() else { throw Errors.apiKeySetIsEmpty }
+        
+        let headers = [
+            "X-RapidAPI-Key": apiKey,
+            "X-RapidAPI-Host": "basketapi1.p.rapidapi.com"
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+        
+        let (response, _) = try await URLSession.shared.data(for: request)
+        
+        return response
     }
 }
