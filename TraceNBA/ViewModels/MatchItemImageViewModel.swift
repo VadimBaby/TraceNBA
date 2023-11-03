4566a9f0c7ecdc7bd4c2204a01bea13fec4b883f
VadimBaby <vadimdim22@gmail.com>
2023-11-03 13:06:01 +0700

    Turned func getTeamImage to async func

--- TraceNBA/ViewModels/MatchItemImageViewModel.swift
+++ TraceNBA/ViewModels/MatchItemImageViewModel.swift
@@ -16,44 +16,33 @@ class MatchItemImageViewModel: ObservableObject {
     
     private let localFileManager = LocalFileManager.instance
     
-    private var tasks: [Task<Void, Never>] = []
-    
     init(dataService: DataServiceProtocol) {
         self.manager = dataService
     }
     
-    func getTeamImage(id: Int) {
+    func getTeamImage(id: Int) async {
         let uiImage: UIImage? = localFileManager.getImage(name: "\(id)", typeFolder: .teams)
         
         if let uiImage {
             image = uiImage
         } else {
-            getTeamImagefromDataBase(id: id)
+            await getTeamImagefromDataBase(id: id)
         }
     }
     
-    private func getTeamImagefromDataBase(id: Int) {
-        let task1 = Task {
-            do {
-                let dataImage = try await manager.getPhotoEntity(entity: .team, id: id)
-                
-                guard let uiImage: UIImage = UIImage(data: dataImage) else { throw Errors.badImage }
-                
-                await MainActor.run {
-                    localFileManager.saveImage(image: uiImage, name: "\(id)", typeFolder: .teams)
-                    self.image = uiImage
-                }
-                
-            } catch {
-                print(error.localizedDescription)
+    private func getTeamImagefromDataBase(id: Int) async {
+        do {
+            let dataImage = try await manager.getPhotoEntity(entity: .team, id: id)
+            
+            guard let uiImage: UIImage = UIImage(data: dataImage) else { throw Errors.badImage }
+            
+            await MainActor.run {
+                localFileManager.saveImage(image: uiImage, name: "\(id)", typeFolder: .teams)
+                self.image = uiImage
             }
+            
+        } catch {
+            print(error.localizedDescription)
         }
-        
-        tasks.append(task1)
-    }
-    
-    func cancelAllTasks() {
-        tasks.forEach{ $0.cancel() }
-        tasks = []
     }
 }
