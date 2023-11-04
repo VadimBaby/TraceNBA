7d3032a143453d5bd9dad51060f53cd37ca26e3c
VadimBaby <vadimdim22@gmail.com>
2023-11-04 23:08:00 +0700

    Removed CustomTabView

--- TraceNBA/Views/StreamMatchesView.swift
+++ TraceNBA/Views/StreamMatchesView.swift
@@ -48,43 +48,56 @@ struct StreamMatchesView: View {
 }
 
 #Preview {
-    ZStack {
-        LinearGradient(gradient: Gradient(colors: [Color("FirstGradientColor"), Color("SecondGradientColor")]), startPoint: .leading, endPoint: .trailing)
-            .ignoresSafeArea()
+    TabView(selection: .constant(2)) {
+        Text("Home")
+            .tabItem {
+                VStack {
+                    Text("Home")
+                    Image(systemName: "house")
+                }
+            }
+            .tag(0)
         
-        CustomTabBarContainerView(
-            selection: .constant(TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right")),
-            iconColor: Color.green
-        ) {
-            Text("Home")
-                .tabBarItem (
-                    tab: TabItem(title: "Home", iconName: "house"),
-                    selection: .constant(TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"))
-                )
-            
-            Text("Search")
-                .tabBarItem (
-                    tab: TabItem(title: "Search", iconName: "magnifyingglass"),
-                    selection: .constant(TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"))
-                )
-            
-            StreamMatchesView(dataService: DataService())
-                .tabBarItem (
-                    tab: TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"),
-                    selection: .constant(TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"))
-                )
-            
-            Text("Events")
-                .tabBarItem (
-                    tab: TabItem(title: "Events", iconName: "calendar"),
-                    selection: .constant(TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"))
-                )
-            
-            Text("Settings")
-                .tabBarItem (
-                    tab: TabItem(title: "Settings", iconName: "gear"),
-                    selection: .constant(TabItem(title: "Streams", iconName: "dot.radiowaves.left.and.right"))
-                )
-        }
+        Text("Search")
+            .tabItem {
+                VStack {
+                    Text("Search")
+                    Image(systemName: "magnifyingglass")
+                }
+            }
+            .tag(1)
+        
+        StreamMatchesView(dataService: DataService())
+            .tabItem {
+                VStack {
+                    Text("Streams")
+                    Image(systemName: "dot.radiowaves.left.and.right")
+                }
+            }
+            .tag(2)
+        
+        Text("Events")
+            .tabItem {
+                VStack {
+                    Text("Events")
+                    Image(systemName: "calendar")
+                }
+            }
+            .tag(3)
+        
+        Text("Settings")
+            .tabItem {
+                VStack {
+                    Text("Settings")
+                    Image(systemName: "gear")
+                }
+            }
+            .tag(4)
+    }
+    .onAppear {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithDefaultBackground()
+        UITabBar.appearance().standardAppearance = appearance
+        UITabBar.appearance().scrollEdgeAppearance = appearance
     }
 }
