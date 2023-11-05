ac23862af7591a859956f3e5c14b570fd839d351
VadimBaby <vadimdim22@gmail.com>
2023-11-05 14:14:43 +0700

    Added var dateSchedule, func addDayToDateSchedule, func minusDayToDateSchedule

--- TraceNBA/ViewModels/EventsViewModel.swift
+++ TraceNBA/ViewModels/EventsViewModel.swift
@@ -9,9 +9,27 @@ import Foundation
 
 final class EventsViewModel: ObservableObject {
     
+    @Published private(set) var dateSchedule: Date = Date()
+    
     private let dataService: DataServiceProtocol
     
     init(dataService: DataServiceProtocol) {
         self.dataService = dataService
+    }
+    
+    func addDayToDateSchedule() {
+        let newDate = Calendar.current.date(byAdding: .day, value: 1, to: dateSchedule)
+        
+        guard let newDate else { return }
+        
+        dateSchedule = newDate
+    }
+    
+    func minusDayToDateSchedule() {
+        let newDate = Calendar.current.date(byAdding: .day, value: -1, to: dateSchedule)
+        
+        guard let newDate else { return }
+        
+        dateSchedule = newDate
     }
 }
