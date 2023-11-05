e141eefc3eceed7a99346817455880476d414c14
VadimBaby <vadimdim22@gmail.com>
2023-11-05 13:51:12 +0700

    Created ChangingDateViewComponent

---
+++ TraceNBA/Components/ChangingDateViewComponent.swift
@@ -0,0 +1,22 @@
+//
+//  ChangingDateViewComponent.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 05.11.2023.
+//
+
+import SwiftUI
+
+struct ChangingDateViewComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ZStack {
+        GradientComponent()
+        
+        ChangingDateViewComponent()
+    }
+}
