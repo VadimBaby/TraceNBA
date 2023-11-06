5036326b5cab375bf6431e5501b25012f0b88ccc
VadimBaby <vadimdim22@gmail.com>
2023-11-07 00:52:48 +0700

    Added name parameter in StatisticsItemModel

--- TraceNBA/Models/StatisticsItemModel.swift
+++ TraceNBA/Models/StatisticsItemModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 struct StatisticsItemModel: Codable {
+    let name: NameStatisticsItemType
     let home: String
     let away: String
     let valueType: ValueType
@@ -16,7 +17,7 @@ struct StatisticsItemModel: Codable {
     let homeTotal: Int?
     let awayTotal: Int?
     
-    init(home: String, away: String, valueType: String, homeValue: Int, awayValue: Int, homeTotal: Int?, awayTotal: Int?) {
+    init(name: String, home: String, away: String, valueType: String, homeValue: Int, awayValue: Int, homeTotal: Int?, awayTotal: Int?) {
         self.home = home
         self.away = away
         self.homeValue = homeValue
@@ -25,8 +26,10 @@ struct StatisticsItemModel: Codable {
         self.awayTotal = awayTotal
         
         let valueTypeOptional = ValueType(rawValue: valueType)
+        let nameOptional = NameStatisticsItemType(rawValue: name)
         
         self.valueType = valueTypeOptional ?? .unknown
+        self.name = nameOptional ?? .unknown
     }
 
 }
