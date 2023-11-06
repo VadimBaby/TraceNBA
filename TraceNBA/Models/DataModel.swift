9b45f503ee620fb0e18491774b820ea544d0640c
VadimBaby <vadimdim22@gmail.com>
2023-11-07 00:53:09 +0700

    Added statistics parameter to DataModel

--- TraceNBA/Models/DataModel.swift
+++ TraceNBA/Models/DataModel.swift
@@ -10,24 +10,34 @@ import Foundation
 struct DataModel: Codable {
     let events: [MatchModel]?
     let players: [PlayerDataModel]?
+    let statistics: [StatisticsMatchModel]?
     let message: String?
     
     init(events: [MatchModel]) {
         self.events = events
         self.players = nil
         self.message = nil
+        self.statistics = nil
     }
     
     init(players: [PlayerDataModel]) {
         self.players = players
         self.events = nil
         self.message = nil
+        self.statistics = nil
     }
     
+    init(statistics: [StatisticsMatchModel]) {
+        self.statistics = statistics
+        self.events = nil
+        self.players = nil
+        self.message = nil
+    }
     
     init(message: String) {
         self.message = message
         self.players = nil
         self.events = nil
+        self.statistics = nil
     }
 }
