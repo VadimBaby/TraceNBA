b66173ba2695c940b8b68bd10530878f41715a34
VadimBaby <vadimdim22@gmail.com>
2023-11-08 12:20:55 +0700

    Added var statistics and added private func getStatisticsMatchData in StatisticsMatchViewModel

--- TraceNBA/ViewModels/StatisticsMatchViewModel.swift
+++ TraceNBA/ViewModels/StatisticsMatchViewModel.swift
@@ -8,9 +8,21 @@
 import Foundation
 
 class StatisticsMatchViewModel: ObservableObject {
+    @Published private(set) var statistics: [StatisticsMatchModel] = []
+    
     let dataService: DataServiceProtocol
     
     init(dataService: DataServiceProtocol) {
         self.dataService = dataService
+    }
+    
+    private func getStatisticsMatchData(id: Int, isRefresh: Bool) async throws -> [StatisticsMatchModel] {
+        let data = try await dataService.getStatisticsMatchData(id: id, isRefresh: isRefresh)
+        
+        let decodeData = try JSONDecoder().decode(DataModel.self, from: data)
+        
+        guard let statistics = decodeData.statistics else { throw URLError(.badServerResponse) }
+        
+        return statistics
     }
 }
