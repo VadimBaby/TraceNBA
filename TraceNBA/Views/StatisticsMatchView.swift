b1c342897a1b76a695805577b189095315198040
VadimBaby <vadimdim22@gmail.com>
2023-11-08 19:13:15 +0700

    Added let dataService and set GradientComponent as background in StatisticsMatchView, StatisticsMatchView embed in NavigationStack inside #Preview

--- TraceNBA/Views/StatisticsMatchView.swift
+++ TraceNBA/Views/StatisticsMatchView.swift
@@ -12,46 +12,55 @@ struct StatisticsMatchView: View {
     
     @StateObject private var viewModel: StatisticsMatchViewModel
     
+    private let dataService: DataServiceProtocol
+    
     init(matchModel: MatchModel, dataService: DataServiceProtocol) {
         self.matchModel = matchModel
         self._viewModel = StateObject(wrappedValue: StatisticsMatchViewModel(dataService: dataService))
+        self.dataService = dataService
     }
     
     var body: some View {
-        Text("Statistics")
+        ZStack {
+            GradientComponent()
+            
+            
+        }
     }
 }
 
 #Preview {
-    StatisticsMatchView(matchModel: MatchModel(
-        id: 11548663,
-        tournament: nil,
-        homeTeam: TeamModel(
-            id: 3428,
-            name: "Golden State Warriors",
-            shortName: "Warriors",
-            nameCode: "GSW"),
-        awayTeam: TeamModel(
-            id: 3413,
-            name: "Sacramento Kings",
-            shortName: "Kings",
-            nameCode: "SAC"),
-        homeScore: ScoreModel(
-            current: 86,
-            display: 86,
-            period1: 28,
-            period2: 28,
-            period3: 18,
-            period4: 12
-        ),
-        awayScore: ScoreModel(
-            current: 85,
-            display: 85,
-            period1: 31,
-            period2: 31,
-            period3: 24,
-            period4: 6
-        ),
-        startTimestamp: 1698890400
-    ), dataService: MockDataService<DataModel>())
+    NavigationStack {
+        StatisticsMatchView(matchModel: MatchModel(
+            id: 11548663,
+            tournament: nil,
+            homeTeam: TeamModel(
+                id: 3428,
+                name: "Golden State Warriors",
+                shortName: "Warriors",
+                nameCode: "GSW"),
+            awayTeam: TeamModel(
+                id: 3413,
+                name: "Sacramento Kings",
+                shortName: "Kings",
+                nameCode: "SAC"),
+            homeScore: ScoreModel(
+                current: 86,
+                display: 86,
+                period1: 28,
+                period2: 28,
+                period3: 18,
+                period4: 12
+            ),
+            awayScore: ScoreModel(
+                current: 85,
+                display: 85,
+                period1: 31,
+                period2: 31,
+                period3: 24,
+                period4: 6
+            ),
+            startTimestamp: 1698890400
+        ), dataService: MockDataService<DataModel>())
+    }
 }
