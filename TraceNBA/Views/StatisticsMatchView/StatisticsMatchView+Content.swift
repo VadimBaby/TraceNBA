f68ec8bb9d0b432c0afad070f4d8d1f013161bcf
VadimBaby <vadimdim22@gmail.com>
2023-11-11 11:24:57 +0700

    added preview in StatisticsMatchView+Components and StatisticsMatchView+Content

--- TraceNBA/Views/StatisticsMatchView/StatisticsMatchView+Content.swift
+++ TraceNBA/Views/StatisticsMatchView/StatisticsMatchView+Content.swift
@@ -55,3 +55,39 @@ extension StatisticsMatchView {
         }
     }
 }
+
+#Preview {
+    NavigationStack {
+        StatisticsMatchView(matchModel: MatchModel(
+            id: 11548663,
+            tournament: nil,
+            homeTeam: TeamModel(
+                id: 3428,
+                name: "Golden State Warriors",
+                shortName: "Warriors",
+                nameCode: "GSW"),
+            awayTeam: TeamModel(
+                id: 3413,
+                name: "Sacramento Kings",
+                shortName: "Kings",
+                nameCode: "SAC"),
+            homeScore: ScoreModel(
+                current: 86,
+                display: 86,
+                period1: 28,
+                period2: 28,
+                period3: 18,
+                period4: 12
+            ),
+            awayScore: ScoreModel(
+                current: 85,
+                display: 85,
+                period1: 31,
+                period2: 31,
+                period3: 24,
+                period4: 6
+            ),
+            startTimestamp: 1698890400
+        ), dataService: MockDataService<DataModel>())
+    }
+}
