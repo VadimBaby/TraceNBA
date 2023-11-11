da786727d7c22c2b6be00b2b323e821c170532a7
VadimBaby <vadimdim22@gmail.com>
2023-11-11 19:30:53 +0700

    Created TypeStatisticsView

---
+++ TraceNBA/Enums/TypeStatisticsView.swift
@@ -0,0 +1,14 @@
+//
+//  TypeStatisticsView.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 11.11.2023.
+//
+
+import Foundation
+
+enum TypeStatisticsView {
+    case summary
+    case box_score
+    case highlights
+}
