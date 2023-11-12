8e5694a8752fef62ae64c65e5178e2a646a59091
VadimBaby <vadimdim22@gmail.com>
2023-11-12 11:56:22 +0700

    Created CountryModel

---
+++ TraceNBA/Models/CountryModel.swift
@@ -0,0 +1,13 @@
+//
+//  CountryModel.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 12.11.2023.
+//
+
+import Foundation
+
+struct CountryModel: Codable {
+    let name: String
+    let alpha2: String
+}
