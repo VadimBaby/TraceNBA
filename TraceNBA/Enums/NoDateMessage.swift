97823e58a4d153bd04188ca907efd25d15588cd8
VadimBaby <vadimdim22@gmail.com>
2023-11-12 13:06:21 +0700

    Added case noMatchLineups to enum NoDataMessage

--- TraceNBA/Enums/NoDateMessage.swift
+++ TraceNBA/Enums/NoDateMessage.swift
@@ -11,4 +11,5 @@ enum NoDateMessage: String {
     case noLiveMatches = "Unfortunately, none of the teams aren't playing now"
     case noScheduleMatches = "Unfortunately, there is no match schedule for this day"
     case noStatisticsMatch = "Unfortunately, the statistics of this match are not available yet"
+    case noMatchLineups = "Unfortunately, the lineups of this match are not available yet"
 }
