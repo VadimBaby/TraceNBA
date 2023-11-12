a5a084c428a09eba9903d2863ac47d291b391fcd
VadimBaby <vadimdim22@gmail.com>
2023-11-12 23:43:42 +0700

    Created HighlightsImageViewComponent

---
+++ TraceNBA/Components/StatisticsMatchView/HighlightsImageViewComponent.swift
@@ -0,0 +1,49 @@
+//
+//  HighlightsImageViewComponent.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 12.11.2023.
+//
+
+import SwiftUI
+
+struct HighlightsImageViewComponent: View {
+    
+    @StateObject private var viewModel: ImageViewComponetViewModel
+    
+    let urlString: String
+    
+    init(urlString: String, dataService: DataServiceProtocol) {
+        self.urlString = urlString
+        self._viewModel = StateObject(wrappedValue: ImageViewComponetViewModel(dataService: dataService))
+    }
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .frame(maxWidth: .infinity)
+                    .scaledToFit()
+            } else {
+                VStack {
+                    ProgressView()
+                        .tint(Color.white)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 200)
+            }
+        }
+        .task {
+            await viewModel.getHighlightImage(urlString: urlString)
+        }
+    }
+}
+
+#Preview {
+    ZStack {
+        GradientComponent()
+        
+        HighlightsImageViewComponent(urlString: FakeData.fakeMatchHighlights.first!.thumbnailUrl, dataService: DataService())
+    }
+}
