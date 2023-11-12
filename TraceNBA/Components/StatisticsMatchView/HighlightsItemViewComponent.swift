5752e3fcd2feb1cab8648d980b5e30807234e84d
VadimBaby <vadimdim22@gmail.com>
2023-11-12 23:48:47 +0700

    VStack put inside Link in HighlightsItemViewComponent

--- TraceNBA/Components/StatisticsMatchView/HighlightsItemViewComponent.swift
+++ TraceNBA/Components/StatisticsMatchView/HighlightsItemViewComponent.swift
@@ -13,20 +13,26 @@ struct HighlightsItemViewComponent: View {
     let dataService: DataServiceProtocol
     
     var body: some View {
-        VStack {
-            HighlightsImageViewComponent(
-                urlString: highlight.thumbnailUrl,
-                dataService: dataService
-            )
-            Text(highlight.title.uppercased())
-                .foregroundStyle(Color.white)
-                .font(.title2)
-                .fontWeight(.medium)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            Text(highlight.subtitle.uppercased())
-                .font(.headline)
-                .foregroundStyle(Color(uiColor: UIColor.lightText))
-                .frame(maxWidth: .infinity, alignment: .leading)
+        if let url = URL(string: highlight.url) {
+            Link(destination: url, label: {
+                VStack {
+                    HighlightsImageViewComponent(
+                        urlString: highlight.thumbnailUrl,
+                        dataService: dataService
+                    )
+                    Text(highlight.title.uppercased())
+                        .foregroundStyle(Color.white)
+                        .font(.title2)
+                        .fontWeight(.medium)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .multilineTextAlignment(.leading)
+                    Text(highlight.subtitle.uppercased())
+                        .font(.headline)
+                        .foregroundStyle(Color(uiColor: UIColor.lightText))
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .multilineTextAlignment(.leading)
+                }
+            })
         }
     }
 }
