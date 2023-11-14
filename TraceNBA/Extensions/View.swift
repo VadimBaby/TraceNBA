9c2e6f61519edf9c3e04aee85cdfc3ed63d3793c
VadimBaby <vadimdim22@gmail.com>
2023-11-14 11:51:01 +0700

    Created func onScrollViewOffsetChanged in View

---
+++ TraceNBA/Extensions/View.swift
@@ -0,0 +1,24 @@
+//
+//  View.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 14.11.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension View {
+    func onScrollViewOffsetChanged(action: @escaping (_ value: CGFloat) -> Void) -> some View {
+        self
+            .background {
+                GeometryReader { geo in
+                    Text("")
+                        .preference(key: ScrollViewOffsetPreferenceKey.self, value: geo.frame(in: .global).minY)
+                }
+            }
+            .onPreferenceChange(ScrollViewOffsetPreferenceKey.self, perform: { value in
+                action(value)
+            })
+    }
+}
