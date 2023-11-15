ac20321720bce08e9f1cb963215df36b6658a052
VadimBaby <vadimdim22@gmail.com>
2023-11-15 13:17:09 +0700

    Created enum TypeIncidentClass

---
+++ TraceNBA/Enums/TypeIncidentClass.swift
@@ -0,0 +1,15 @@
+//
+//  TypeIncidentClass.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 15.11.2023.
+//
+
+import Foundation
+
+enum TypeIncidentClass: String {
+    case onePoint
+    case twoPoints
+    case threePoints
+    case unknown = "Unknown NameStatisticsItemType"
+}
