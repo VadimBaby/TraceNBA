dd234bf640ee04d6d871c6c41c726bad8d9d4322
VadimBaby <vadimdim22@gmail.com>
2023-11-18 18:25:24 +0700

    Created PlayerStatisticsDataModel

---
+++ TraceNBA/Models/PlayerStatisticsDataModel.swift
@@ -0,0 +1,12 @@
+//
+//  PlayerStatisticsData.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 18.11.2023.
+//
+
+import Foundation
+
+struct PlayerStatisticsDataModel: Codable {
+    let statistics: PlayerStatisticsModel
+}
