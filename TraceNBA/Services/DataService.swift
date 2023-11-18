2e0d0862eaa416857450326cf53b3ddd39aadb4c
VadimBaby <vadimdim22@gmail.com>
2023-11-18 20:44:28 +0700

    Added func getPlayerStatisticsRegualarSeason to DataService

--- TraceNBA/Services/DataService.swift
+++ TraceNBA/Services/DataService.swift
@@ -143,6 +143,12 @@ actor DataService: DataServiceProtocol {
         return try await getDataFromUrl(urlString: urlString, isRefresh: isRefresh)
     }
     
+    func getPlayerStatisticsRegualarSeason(idPlayer: Int, idSeason: Int, isRefresh: Bool) async throws -> Data {
+        let urlString = "https://basketapi1.p.rapidapi.com/api/basketball/player/\(idPlayer)/tournament/\(Constants.tournament)/season/\(idSeason)/statistics/regularseason"
+        
+        return try await getDataFromUrl(urlString: urlString, isRefresh: isRefresh)
+    }
+    
     private func getDataFromUrl(urlString: String, isRefresh: Bool = false) async throws -> Data {
         
         guard let url = URL(string: urlString) else { throw URLError(.badURL) }
