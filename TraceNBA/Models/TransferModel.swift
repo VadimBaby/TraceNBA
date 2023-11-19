6308b476147088124f72adf92efdcbbec18ae313
VadimBaby <vadimdim22@gmail.com>
2023-11-19 21:44:06 +0700

    Added init to TransferModel

--- TraceNBA/Models/TransferModel.swift
+++ TraceNBA/Models/TransferModel.swift
@@ -17,4 +17,16 @@ struct TransferModel: Codable, Identifiable {
     let pick: Int?
     let id: Int
     let transferDateTimestamp: Int
+    
+    init(player: PlayerModel, transferFrom: TeamModel? = nil, transferTo: TeamModel? = nil, fromTeamName: String, toTeamName: String, round: Int? = nil, pick: Int? = nil, id: Int, transferDateTimestamp: Int) {
+        self.player = player
+        self.transferFrom = transferFrom
+        self.transferTo = transferTo
+        self.fromTeamName = fromTeamName
+        self.toTeamName = toTeamName
+        self.round = round
+        self.pick = pick
+        self.id = id
+        self.transferDateTimestamp = transferDateTimestamp
+    }
 }
