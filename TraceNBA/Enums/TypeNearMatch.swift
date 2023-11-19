4e3df1051319d3d588b1c0dc359968f62f2ca912
VadimBaby <vadimdim22@gmail.com>
2023-11-19 20:22:54 +0700

    Created enum TypeNearMatch

---
+++ TraceNBA/Enums/TypeNearMatch.swift
@@ -0,0 +1,13 @@
+//
+//  TypeNearMatch.swift
+//  TraceNBA
+//
+//  Created by Вадим Мартыненко on 19.11.2023.
+//
+
+import Foundation
+
+enum TypeNearMatch: String {
+    case nextMatch = "Next Match"
+    case previousEvent = "Previous Match"
+}
