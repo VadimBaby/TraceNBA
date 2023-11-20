e8f15605d01036db05a5e6ec873294d11ba4f672
VadimBaby <vadimdim22@gmail.com>
2023-11-20 18:02:36 +0700

    Added var media to PlayerProfileViewModel

--- TraceNBA/ViewModels/PlayerProfileViewModel.swift
+++ TraceNBA/ViewModels/PlayerProfileViewModel.swift
@@ -16,6 +16,7 @@ class PlayerProfileViewModel: ObservableObject {
     @Published private(set) var previousEvent: MatchModel? = nil
     @Published private(set) var nextEvent: MatchModel? = nil
     @Published private(set) var transferHistory: [TransferModel]? = nil
+    @Published private(set) var media: [HighlightModel]? = nil
     @Published private(set) var hasPlayerStatisticsError: Bool = false
     @Published private(set) var hasNearEventError: Bool = false
     
