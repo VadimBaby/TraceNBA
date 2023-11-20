e2bec08d485b61983cee704fc8b1815f2114e991
VadimBaby <vadimdim22@gmail.com>
2023-11-20 18:06:27 +0700

    Added func getPlayerMedia to PlayerProfileViewModel

--- TraceNBA/ViewModels/PlayerProfileViewModel.swift
+++ TraceNBA/ViewModels/PlayerProfileViewModel.swift
@@ -105,6 +105,14 @@ class PlayerProfileViewModel: ObservableObject {
         tasks.append(task)
     }
     
+    func getPlayerMedia() {
+        let task = Task {
+            await asyncGetPlayerMedia(isRefresh: false)
+        }
+        
+        tasks.append(task)
+    }
+    
     func asyncGetPlayerDetails(isRefresh: Bool) async {
         await MainActor.run {
             self.player = nil
