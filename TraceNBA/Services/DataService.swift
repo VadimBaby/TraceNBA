2fb1c1649aab0a919e5f9aebacc5db623763a96b
VadimBaby <vadimdim22@gmail.com>
2023-11-20 20:08:42 +0700

    Added func getTeamDetails to DataService

--- TraceNBA/Services/DataService.swift
+++ TraceNBA/Services/DataService.swift
@@ -167,6 +167,12 @@ actor DataService: DataServiceProtocol {
         return try await getDataFromUrl(urlString: urlString, isRefresh: isRefresh)
     }
     
+    func getTeamDetails(id: Int, isRefresh: Bool) async throws -> Data {
+        let urlString = "https://basketapi1.p.rapidapi.com/api/basketball/team/\(id)"
+        
+        return try await getDataFromUrl(urlString: urlString, isRefresh: isRefresh)
+    }
+    
     private func getDataFromUrl(urlString: String, isRefresh: Bool = false) async throws -> Data {
         
         guard let url = URL(string: urlString) else { throw URLError(.badURL) }
