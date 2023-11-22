5835405e73a53a13abdc50ea1684d090fa25ce1f
VadimBaby <vadimdim22@gmail.com>
2023-11-22 12:50:30 +0700

    Added func searchData to SearchViewModel

--- TraceNBA/ViewModels/SearchViewModel.swift
+++ TraceNBA/ViewModels/SearchViewModel.swift
@@ -14,4 +14,27 @@ class SearchViewModel: ObservableObject {
     init(dataService: DataServiceProtocol) {
         self.dataService = dataService
     }
+    
+    private func searchData(search: String, isRefresh: Bool) async throws -> [SearchModel] {
+        guard !search.isEmpty else { throw Errors.searchIsEmpty }
+        
+        let data = try await dataService.search(search: search, isRefresh: isRefresh)
+        
+        let decodeData = try JSONDecoder().decode(DataModel.self, from: data)
+        
+        guard let results = decodeData.results else { throw Errors.dataIsNil }
+        
+        return results.filter { item in
+            if item.type == .team {
+                return item.entity.country.name == "USA"
+            } else if item.type == .player {
+                guard let team = item.entity.team,
+                      let country = team.country else { return false }
+                
+                return country.name == "USA"
+            }
+            
+            return false
+        }
+    }
 }
